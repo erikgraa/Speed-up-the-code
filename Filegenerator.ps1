974b946ceccd6b7b1ea7bdd33f2b17f8991e9101
Erik <20067955+erikgraa@users.noreply.github.com>
2025-06-24 01:45:49 +0200

    Update Filegenerator.ps1
    * Changed Get-Random to [System.Random]
    * $logLines captures the entire loop output

--- Filegenerator.ps1
+++ Filegenerator.ps1
@@ -8,33 +8,41 @@ $errorTypes = @(
     'Temperature warning'
 )
 $statusCodes = 'OK','WARN','ERR'
- 
-$logLines = @()
- 
-for ($i=0; $i -lt 50000; $i++) {
+
+#$d = get-date
+$logLines = for ($i=0; $i -lt 50000; $i++) {
+    #$timestamp = $d.AddSeconds(-$i).ToString("yyyy-MM-dd HH:mm:ss")
     $timestamp = (Get-Date).AddSeconds(-$i).ToString("yyyy-MM-dd HH:mm:ss")
-    $plc = $plcNames | Get-Random
-    $operator = Get-Random -Minimum 101 -Maximum 121
-    $batch = Get-Random -Minimum 1000 -Maximum 1101
-    $status = $statusCodes | Get-Random
-    $machineTemp = [math]::Round((Get-Random -Minimum 60 -Maximum 110) + (Get-Random),2)
-    $load = Get-Random -Minimum 0 -Maximum 101
+    #$plc = $plcNames | Get-Random
+    $plc = Get-Random -InputObject $plcNames
+    #$operator = Get-Random -Minimum 101 -Maximum 121
+    $operator = [System.Random]::New().Next(101,121)
+    #$batch = Get-Random -Minimum 1000 -Maximum 1101
+    $batch = [System.Random]::New().Next(1000,1101)
+    #$status = $statusCodes | Get-Random
+    $status = Get-Random -InputObject $statusCodes
+    #$machineTemp = [math]::Round((Get-Random -Minimum 60 -Maximum 110) + (Get-Random),2)
+    $machineTemp = [math]::Round([System.Random]::New().Next(60,110)+ [System.Random]::New().Next(),2)
+    #$load = Get-Random -Minimum 0 -Maximum 101
+    $load = [System.Random]::New().Next(0,101)
  
-    if ((Get-Random -Minimum 1 -Maximum 8) -eq 4) {
-        $errorType = $errorTypes | Get-Random
+    #if ((Get-Random -Minimum 1 -Maximum 8) -eq 4) {
+    if (([System.Random]::New().Next(1,8)) -eq 4) {
+        #$errorType = $errorTypes | Get-Random 
+        $errorType = Get-Random -InputObject $errorTypes
         if ($errorType -eq 'Sandextrator overload') {
-            $value = (Get-Random -Minimum 1 -Maximum 11)
-            $msg = "ERROR; $timestamp; $plc; $errorType; $value; $status; $operator; $batch; $machineTemp; $load"
+            #$value = (Get-Random -Minimum 1 -Maximum 11)
+            $value = [System.Random]::New().Next(1,11)
+            "ERROR; $timestamp; $plc; $errorType; $value; $status; $operator; $batch; $machineTemp; $load"
         } else {
-            $msg = "ERROR; $timestamp; $plc; $errorType; ; $status; $operator; $batch; $machineTemp; $load"
+            "ERROR; $timestamp; $plc; $errorType; ; $status; $operator; $batch; $machineTemp; $load"
         }
     } else {
-        $msg = "INFO; $timestamp; $plc; System running normally; ; $status; $operator; $batch; $machineTemp; $load"
+        "INFO; $timestamp; $plc; System running normally; ; $status; $operator; $batch; $machineTemp; $load"
     }
- 
-    $logLines += $msg
 }
  
 Set-Content -Path $bigFileName -Value $logLines
+#[System.IO.File]::WriteAllText($bigFileName, $logLines)
 Write-Output "PLC log file generated."
 }
